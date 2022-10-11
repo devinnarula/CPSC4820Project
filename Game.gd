f8484a8a08ebad2fefbb46231f678f6e1d290e54
Devin <devin.j.narula@gmail.com>
2022-10-11 10:40:18 -0400

    Starting health bars

--- Game.gd
+++ Game.gd
@@ -18,12 +18,13 @@ var curr_player = 0
 func _ready():
 	rng.randomize()
 	call_deferred("setup_game")
+	players[0].updateHealth(-10, $Player1Health)
 
 func setup_game():
 	$Label.set_text(players[curr_player].player_name);
-	var cells = $TileMap.get_used_cells()
+	var cells = $TileMap1.get_used_cells()
 	for cell in cells:
-		var index = $TileMap.get_cell(cell.x, cell.y)
+		var index = $TileMap1.get_cell(cell.x, cell.y)
 		match index:
 			CLICK_TO_ROLL:
 				#roll_dice(cell, self)
@@ -33,13 +34,13 @@ func roll_dice(coord:Vector2):
 	dice_animation(coord)
 	var dice = rng.randi_range(0,5)
 	moves = dice+1
-	$TileMap.set_cell(coord.x, coord.y, DICE[dice])
+	$TileMap1.set_cell(coord.x, coord.y, DICE[dice])
 	
 func dice_animation(coord:Vector2):	
 	for i in range(0,20):
 		var dice = rng.randi_range(0,5)
 		moves = dice+1
-		$TileMap.set_cell(coord.x, coord.y, DICE[dice])
+		$TileMap1.set_cell(coord.x, coord.y, DICE[dice])
 		yield(get_tree().create_timer(0.05), "timeout")
 
 func next_turn(coord:Vector2):
@@ -48,12 +49,12 @@ func next_turn(coord:Vector2):
 	else:
 		curr_player=0
 	$Label.set_text(players[curr_player].player_name);
-	$TileMap.set_cell(coord.x, coord.y, CLICK_TO_ROLL)
+	$TileMap1.set_cell(coord.x, coord.y, CLICK_TO_ROLL)
 	
 func _input(event):
 	if event is InputEventMouseButton:
-		var click = $TileMap.world_to_map($TileMap.to_local(event.position)) 
-		if 0<=click.x-DICE_CELL.x && click.x-DICE_CELL.x<=3 && 0<=click.y-DICE_CELL.y && click.y-DICE_CELL.y<=3&& $TileMap.get_cell(DICE_CELL.x, DICE_CELL.y) == CLICK_TO_ROLL:
+		var click = $TileMap1.world_to_map($TileMap1.to_local(event.position)) 
+		if 0<=click.x-DICE_CELL.x && click.x-DICE_CELL.x<=3 && 0<=click.y-DICE_CELL.y && click.y-DICE_CELL.y<=3&& $TileMap1.get_cell(DICE_CELL.x, DICE_CELL.y) == CLICK_TO_ROLL:
 			roll_dice(DICE_CELL)
 	if event is InputEventKey and event.pressed and not event.is_echo():
 		if event.scancode == KEY_UP || event.scancode ==  KEY_DOWN || event.scancode ==  KEY_LEFT || event.scancode ==  KEY_RIGHT:
--- Player.gd
+++ Player.gd
@@ -3,6 +3,11 @@ extends Node2D
 class_name Player
 
 var player_name = ""
-
+var hitpoints = 100
+	
 func _init(name:String):
 	player_name = name
+
+func updateHealth(change:int, bar:ProgressBar):
+	hitpoints += change
+	bar.value = hitpoints
